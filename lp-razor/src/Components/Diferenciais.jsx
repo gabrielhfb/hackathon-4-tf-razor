442e61fb9f265f4dcb5428c48710726a35bf253b
Gabriel Henrique Fiszczuk Brandeburski <brandeburskigabrielhenrique@gmail.com>
2022-10-28 20:09:39 -0300

    Adicionado carousel da Prodigy

--- lp-razor/src/Components/Diferenciais.jsx
+++ lp-razor/src/Components/Diferenciais.jsx
@@ -1,76 +1,78 @@
 import prodigy from "../Assets/img/prodigy.PNG";
+import CarouselProdigy from "./CarouselProdigy";
 
 function Diferenciais() {
   return (
     <section className="diferenciais">
       <h2 id="detalhes">Workstation PRO Prodigy</h2>
 
-      <img src={prodigy} />
+      <div className="container-descritivo">
+        <img src={prodigy} />
+      </div>
 
-      <h3>Robusta, segura e expansível</h3>
-      <p>
-        A Workstation Pro Prodigy foi projetada para profissionais que precisam
-        de uma máquina potente, capaz de rodar os softwares mais complexos e
-        pesados do mercado.
-      </p>
+      <div>
+        <CarouselProdigy />
+        <h3>Robusta, segura e expansível</h3>
+        <p>
+          A Workstation Pro Prodigy foi projetada para profissionais que
+          precisam de uma máquina potente, capaz de rodar os softwares mais
+          complexos e pesados do mercado.
+        </p>
 
-      <p>
-        Ela é o modelo intermediário da Linha Pro da Razor Computadores, capaz
-        de providenciar velocidade e performance na realização de tarefas como
-        computação gráfica, renderização, simulações e análises robustas.
-      </p>
+        <p>
+          Ela é o modelo intermediário da Linha Pro da Razor Computadores, capaz
+          de providenciar velocidade e performance na realização de tarefas como
+          computação gráfica, renderização, simulações e análises robustas.
+        </p>
 
-      <p>
-        Além disso, seja você um profissional de design, de mídia e
-        entretenimento, do setor de contrução, ou do setor de tecnologia e
-        desenvolvimento, a Linha Pro de Workstations da Razor é capaz de
-        fornecer soluções personalizadas para atender às suas necessidades.
-      </p>
+        <p>
+          Além disso, seja você um profissional de design, de mídia e
+          entretenimento, do setor de contrução, ou do setor de tecnologia e
+          desenvolvimento, a Linha Pro de Workstations da Razor é capaz de
+          fornecer soluções personalizadas para atender às suas necessidades.
+        </p>
 
-      <h3>Processador de Alta Potência</h3>
-      <p>
-        A Workstation Pro Prodigy possui opções de processadores AMD® Ryzen™ de
-        terceira geração e Intel® Core™ de décima geração, permitindo a
-        instalação de até 8 núcleos e 16 threads, garantindo excelente
-        performance no processamento de dados.
-      </p>
+        <h3>Processador de Alta Potência</h3>
+        <p>
+          A Workstation Pro Prodigy possui opções de processadores AMD® Ryzen™
+          de terceira geração e Intel® Core™ de décima geração, permitindo a
+          instalação de até 8 núcleos e 16 threads, garantindo excelente
+          performance no processamento de dados.
+        </p>
 
-      <h3>Placa de Vídeo para Melhor Performance</h3>
-      <p>
-        Escolha entre as placas de vídeo Nvidia® Geforce™ GTX 1660S, RTX 3060,
-        RTX 3070 e RTX 3080 e as placas profissionais Nvidia® Quadro™ P1000,
-        P2200, RTX 4000 e RTX 5000, capazes de fornecer o máximo de performance
-        gráfica.{" "}
-      </p>
+        <h3>Placa de Vídeo para Melhor Performance</h3>
+        <p>
+          Escolha entre as placas de vídeo Nvidia® Geforce™ GTX 1660S, RTX 3060,
+          RTX 3070 e RTX 3080 e as placas profissionais Nvidia® Quadro™ P1000,
+          P2200, RTX 4000 e RTX 5000, capazes de fornecer o máximo de
+          performance gráfica.{" "}
+        </p>
 
-      <h3>Capacidade de Memória e Armazenamento Expansível</h3>
-      <p>
-        Na Workstation Pro Prodigy é possível instalar até 28TB de armazenamento
-        de dados em disco rígido ou até 6 TB em armazenamento All Flash SSD, bem
-        como até 128GB de memória RAM DDR4 para garantir o melhor desempenho
-        para o seu sistema.
-      </p>
+        <h3>Capacidade de Memória e Armazenamento Expansível</h3>
+        <p>
+          Na Workstation Pro Prodigy é possível instalar até 28TB de
+          armazenamento de dados em disco rígido ou até 6 TB em armazenamento
+          All Flash SSD, bem como até 128GB de memória RAM DDR4 para garantir o
+          melhor desempenho para o seu sistema.
+        </p>
 
-      <h3>Desempenho testado e certificado</h3>
-      <p>
-        Os modelos da Linha Pro da Razor, entre eles a Workstation Prodigy,
-        foram submetidos a mais de 1000 horas de testes, com qualidade aprovada
-        para uso em ambientes profissionais altamente exigentes.{" "}
-      </p>
+        <h3>Desempenho testado e certificado</h3>
+        <p>
+          Os modelos da Linha Pro da Razor, entre eles a Workstation Prodigy,
+          foram submetidos a mais de 1000 horas de testes, com qualidade
+          aprovada para uso em ambientes profissionais altamente exigentes.{" "}
+        </p>
 
-      <p>
-        Acesse{" "}
-        <a href="https://razor.com.br/wp-content/uploads/2021/03/ficha_tecnica_prodigy.pdf">
-          aqui
-        </a>{" "}
-        para conferir as demais especificações técnicas deste produto.
-      </p>
+        <p>
+          Acesse{" "}
+          <a href="https://razor.com.br/wp-content/uploads/2021/03/ficha_tecnica_prodigy.pdf">
+            aqui
+          </a>{" "}
+          para conferir as demais especificações técnicas deste produto.
+        </p>
+      </div>
 
       <p>Confira uma visão geral da Workstation Prodigy:</p>
-
-      
-
-      
     </section>
   );
 }
--- lp-razor/src/index.js
+++ lp-razor/src/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css"
 import Popper from "popper.js";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
